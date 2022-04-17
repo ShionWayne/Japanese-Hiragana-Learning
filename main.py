47d4c0100e6a991f14491f3fa03bdbef13545aaf
Pan <dengpan.yuan.cool@gmail.com>
2022-04-17 02:53:04 -0400

    finish 2&3

--- main.py
+++ main.py
@@ -1,6 +1,76 @@
+from email.mime import audio
+import os
 from flask import Flask, render_template
 
+audio_folder = os.path.join('static', 'audio')
+
 app = Flask(__name__)
+app.config['learn_audio'] = os.path.join(audio_folder, 'learn')
+app.config['quiz_2_audio'] = os.path.join(audio_folder, 'quiz/2')
+
+learn_dict = {'a': 'あ','i':'い', 'u': 'う', 'e': 'え', 'o':'お'}
+learn_data = [
+    {
+        'id': 'a',
+        'hiragana': 'あ',
+        'sounds_like': 'ah',
+        'audio': os.path.join(app.config['learn_audio'], 'a.mp3')
+    },
+    {
+        'id': 'i',
+        'hiragana': 'い',
+        'sounds_like': 'e',
+        'audio': os.path.join(app.config['learn_audio'], 'i.mp3')
+    },
+    {
+        'id': 'u',
+        'hiragana': 'う',
+        'sounds_like': 'woo',
+        'audio': os.path.join(app.config['learn_audio'], 'u.mp3')
+    },
+    {
+        'id': 'e',
+        'hiragana': 'え',
+        'sounds_like': 'i',
+        'audio': os.path.join(app.config['learn_audio'], 'e.mp3')
+    },
+    {
+        'id': 'o',
+        'hiragana': 'お',
+        'sounds_like': 'o',
+        'audio': os.path.join(app.config['learn_audio'], 'o.mp3')
+    }
+]
+
+quiz_2_data = [
+    {
+        "q_type": 2,
+        "hiragana": "おい",
+        "roman": "hey",
+        "audio": os.path.join("../" + app.config['quiz_2_audio'], 'oi.mp3')
+    },
+    {
+        "q_type": 2,
+        "hiragana": "うえ",
+        "roman": "up",
+        "audio": os.path.join("../" + app.config['quiz_2_audio'], 'ue.mp3')
+    },
+    {
+        "q_type": 2,
+        "hiragana": "あう",
+        "roman": "Meet",
+        "audio": os.path.join("../" + app.config['quiz_2_audio'], 'au.mp3')
+    }
+]
+
+
+quiz_3_data = [
+    {
+        "q_type": 3,
+        "hiragana": "あおい",
+        "roman": "blue"
+    }
+]
 
 @app.route('/learn/<int:id>')
 def learn(id):
@@ -9,7 +79,9 @@ def learn(id):
 @app.route('/quiz/<int:id>')
 def quiz(id):
     if id == 2:
-        return render_template("quiz_2.html")
+        return render_template("quiz_2.html", data=quiz_2_data, p_id=id)
+    if id == 3:
+        return render_template("quiz_3.html", data=quiz_3_data, p_id=id)
     return "this is quiz {}".format(str(id))
 
 @app.route('/quiz_end')
--- static/quiz_2.js
+++
@@ -1,4 +0,0 @@
-function playMusic(){
-    var music = new Audio('musicfile.mp3');
-    music.play();
-}
