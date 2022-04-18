ef7ae8de3a8de9e5c874559823b86a2402b45626
ShionWayne <37065575+ShionWayne@users.noreply.github.com>
2022-04-17 21:21:09 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from email.mime import audio
 import os
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template
 import os
 
 
+image_folder = os.path.join('static', 'image')
+audio_folder = os.path.join('static', 'audio')
 
 app = Flask(__name__)
-audio_folder = os.path.join('static', 'audio')
+app.config['image_folder'] = image_folder
 app.config['learn_audio'] = os.path.join(audio_folder, 'learn')
 app.config['quiz_2_audio'] = os.path.join(audio_folder, 'quiz/2')
 
@@ -16,53 +18,33 @@ learn_data = [
         'id': 'a',
         'hiragana': 'あ',
         'sounds_like': 'ah',
-        'audio': os.path.join("../"+app.config['learn_audio'], 'a.mp3')
+        'audio': os.path.join(app.config['learn_audio'], 'a.mp3')
     },
     {
         'id': 'i',
         'hiragana': 'い',
         'sounds_like': 'e',
-        'audio': os.path.join("../"+app.config['learn_audio'], 'i.mp3')
+        'audio': os.path.join(app.config['learn_audio'], 'i.mp3')
     },
     {
         'id': 'u',
         'hiragana': 'う',
         'sounds_like': 'woo',
-        'audio': os.path.join("../"+app.config['learn_audio'], 'u.mp3')
+        'audio': os.path.join(app.config['learn_audio'], 'u.mp3')
     },
     {
         'id': 'e',
         'hiragana': 'え',
         'sounds_like': 'i',
-        'audio': os.path.join("../"+app.config['learn_audio'], 'e.mp3')
+        'audio': os.path.join(app.config['learn_audio'], 'e.mp3')
     },
     {
         'id': 'o',
         'hiragana': 'お',
         'sounds_like': 'o',
-        'audio': os.path.join("../"+app.config['learn_audio'], 'o.mp3')
+        'audio': os.path.join(app.config['learn_audio'], 'o.mp3')
     }
 ]
-
-quizzes = [
-    {
-        "id": 1,
-        "type": "drag",
-        "problem_text": "Drag the hiragana to corresponding Romanization:",
-        "problem_and_answer": [
-            {"hiragana": "あい", "Romanization": "uo", "English": "fish"},
-            {"hiragana": "うお", "Romanization": "ie", "English": "home"},
-            {"hiragana": "いえ", "Romanization": "ai", "English": "love"}
-        ]
-    }
-]
-
-user_result ={
-    1: [],
-    2: [],
-    3: [],
-    4: []
-}
 
 quiz_2_data = [
     {
@@ -94,71 +76,31 @@ quiz_3_data = [
     }
 ]
 
+@app.route('/startlearning')
+def start_learn():
+    return render_template('learn_0.html')
+
 @app.route('/learn/<int:id>')
 def learn(id):
+    return "this is learn: {}".format(str(id))
 
-    print(id)
-    for i in range(5):
-        data = learn_data[i]
-        print(data)
-        if data['id'] == id:
-            content = data
-            break
-
-    if i == 0:
-        content['prev'] = 'None'
-    else:
-        content['prev'] = learn_data[i-1]['id']
-
-    if i == 4:
-        content['next'] = 'None'
-    else:
-        content['next'] = learn_data[i+1]['id']
-    print(content)
-
-    return render_template('learn.html', content=content)
-
-@app.route('/quiz/<int:id>', methods=['GET', 'POST'])
+@app.route('/quiz/<int:id>')
 def quiz(id):
-    if id == 1:
-        if request.method == 'GET':
-            return render_template("quiz_1.html", content=quizzes[0])
-        else:
-            json_data = request.get_json()
-            user_result[1].append(json_data)
-            answer = []
-            for element in json_data["user_answer"]:
-                if len(element) == 2:
-                    answer.append(element)
-            print(answer)
-            result = {"correct": "True"}
-
-            if len(answer) != 3:
-                result["correct"] = "False"
-            else:
-                for pair in answer:
-                    for i in range(3):
-                        solution = quizzes[0]["problem_and_answer"][i]
-                        if solution["Romanization"] == pair["Romanization"] and solution["hiragana"] != pair["hiragana"]:
-                            print(solution)
-                            print(pair)
-                            result["correct"] = "False"
-                            break
     if id == 2:
         return render_template("quiz_2.html", data=quiz_2_data, p_id=id)
-    if id == 4:
-        return render_template("quiz_4.html", )
     if id == 3:
         return render_template("quiz_3.html", data=quiz_3_data, p_id=id)
+    return "this is quiz {}".format(str(id))
 
 @app.route('/quiz_end')
 def quiz_end():
-    return render_template('end.html')
+    return "Quiz end"
 
 @app.route('/')
 def hello():
     print(learn_dict)
-    return render_template('home.html')
+    homeimg = os.path.join(app.config['image_folder'], 'homeimg.png')
+    return render_template('home.html', homeimg=homeimg)
 
 if __name__ == '__main__':
    app.run(debug = True)
